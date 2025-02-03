cf0df812c314f70022414c07ebb573d8c91be2ed
UtkarshPandey1601 <up81451@gmail.com>
2025-02-03 22:30:39 +0530

    Update score.jsx

--- src/pages/score.jsx
+++ src/pages/score.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
-
+import bgPic from "/bgPIC.png";
+import logo from "/igtsLOGO.png";
 export default function ScoreScreen() {
   const [scores, setScores] = useState([
     { name: "ABC", score: 100 },
     { name: "DEF", score: 120 },
-    { name: "GHI", score: 90 }
+    { name: "GHI", score: 90 },
+    
+    
   ]);
 
   return (
-    <div>
-      <h1>SCORES</h1>
-        <table>
+    <div className="flex flex-col items-center bg-cover bg-center text-lg text-center" style={{ backgroundImage: `url(${bgPic})` }}
+>
+   <img className="h-[15vh]  mb-8 mt-8" src={logo} alt="Logo" />
+      <h1 className="text-3xl font-bold mb-4">SCORES</h1>
+      <table className="w-3/4 bg-white bg-opacity-70 rounded-lg shadow-md text-black">
         <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Score</th>
+          <tr className="bg-purple-600 text-white">
+            <th className="px-4 py-2">S.No</th>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Score</th>
           </tr>
         </thead>
         <tbody>
           {scores.map((el, id) => (
-            <tr key={id}>
-              <td>{id + 1}</td>
-              <td>{el.name}</td>
-              <td>{el.score}</td>
+            <tr key={id} className="border-b">
+              <td className="px-4 py-2">{id + 1}</td>
+              <td className="px-4 py-2">{el.name}</td>
+              <td className="px-4 py-2">{el.score}</td>
             </tr>
           ))}
         </tbody>
