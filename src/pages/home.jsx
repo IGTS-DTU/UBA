97557a9939029d48b91c3edfa99cb9fd7b96d91d
Divij Mahajan <d.mahajan2004@gmail.com>
2025-02-12 03:19:53 +0530

    attempt

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import logo from "/igtsLOGO.png"
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import bgPic from "/bgPIC.png";
 
 function Home() {
   const handleLogin = async () => {
